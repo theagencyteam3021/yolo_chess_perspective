0ecf7a5cb4e3c2ba94872a8ee1a675288efeb2f1
csolomonson <ctsolomonson@gmail.com>
2023-02-20 12:01:43 -0800

    add code to load model

--- chess.py
+++ chess.py
@@ -9,3 +9,19 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+weights = 'runs/weights/chess.pt'
+device = select_device(0)
+half = device != 'cpu'
+trace = True
+imgsz=640
+
+model = attempt_load(weights, map_location=device)
+stride = int(model.stride.max())  # model stride
+imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+if trace:
+    model = TracedModel(model, device, imgsz)
+
+if half:
+    model.half()  # to FP16
