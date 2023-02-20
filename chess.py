ab83c0f9cc62b88a75aef3406b55ed8f15decd6f
csolomonson <ctsolomonson@gmail.com>
2023-02-20 15:24:40 -0800

    Add camera logic

--- chess.py
+++ chess.py
@@ -1,10 +1,10 @@
-import cv2
+from cv2 import cv2
 import torch
 import torch.backends.cudnn as cudnn
-from numpy import random
+import numpy as np
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
+from utils.datasets import LoadStreams, LoadImages, letterbox
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
@@ -25,3 +25,24 @@ if trace:
 
 if half:
     model.half()  # to FP16
+
+img0 = None
+cap = cv2.VideoCapture(0)
+assert cap.isOpened(), 'Failed to open camera stream'
+
+rect = True
+
+def getBoard():
+    #get image
+    cap.grab()
+    success, im = cap.retrieve()
+    img0 = im if success else 0
+    #resize and convert
+    img = [letterbox(img0, imgsz, auto=rect, stride=stride)[0]]
+    img = np.stack(img, 0)
+    img = img[:, :, :, ::-1].transpose(0, 3, 1, 2)
+    img = np.ascontiguousarray(img)
+
+
+    
+
